ec99b539db0ba30c686521731a3a36bbec37ac71
Orangecrow <wynalazca47@gmail.com>
2024-05-23 15:02:42 +0200

    small build.sh update

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ protoc --cpp_out=. my.proto
 gcc my.grpc.pb.cc -c -o my.grpc.pb.o
 gcc my.pb.cc -c -o my.pb.o
 
-gcc my_client.cpp my.grpc.pb.o my.pb.o -o test --libs pthread grpc++ pthread grpc absl_flags absl_flags_parse
+gcc my_client.cpp my.grpc.pb.o my.pb.o -o test -lpthread -lgrpc++ -lpthread -lgrpc -labsl_flags -labsl_flags_parse
